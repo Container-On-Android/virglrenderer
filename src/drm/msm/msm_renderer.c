Container-On-Android/virglrenderer

commit beab181e65cc4e5d1cc3335a7b81598b81f8b285

drm/msm: Do not ignore lseek() failure
Reported-by: Demi Marie Obenour <demi@invisiblethingslab.com>
Signed-off-by: Rob Clark <robdclark@chromium.org>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1377>

src/drm/msm/msm_renderer.c
@@ -394,9 +394,12 @@ msm_renderer_attach_resource(struct virgl_context *vctx, struct virgl_resource *
 
          /* lseek() to get bo size */
          off_t size = lseek(fd, 0, SEEK_END);
-         if (size < 0)
-            drm_log("lseek failed: %" PRId64 " (%s)", size, strerror(errno));
          close(fd);
+         if (size < 0) {
+            drm_log("lseek failed: %" PRId64 " (%s)", size, strerror(errno));
+            gem_close(fd, handle);
+            return;
+         }
 
          obj = msm_object_create(handle, 0, size);
          if (!obj)

commit df2c1e25fab81a10cc0e99de9ef0b069b06579c0

drm/msm: Call DRM_IOCTL_GEM_CLOSE on correct fd
It failed due to being called on the wrong fd.

Found by Claude Opus 4.8.

Fixes: beab181e65cc ("drm/msm: Do not ignore lseek() failure")
Fixes: e6bab14e9336 ("drm/msm: Add error path gem_close()")
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/1638>

src/drm/msm/msm_renderer.c
@@ -351,13 +351,13 @@ msm_renderer_attach_resource(struct virgl_context *vctx, struct virgl_resource *
          close(fd);
          if (size < 0) {
             drm_err("lseek failed: %" PRId64 " (%s)", size, strerror(errno));
-            gem_close(fd, handle);
+            gem_close(dctx->fd, handle);
             return;
          }
 
          obj = msm_object_create(handle, 0, size);
          if (!obj) {
-            gem_close(fd, handle);
+            gem_close(dctx->fd, handle);
             return;
          }
 